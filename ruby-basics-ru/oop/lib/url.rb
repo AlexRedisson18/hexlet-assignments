52f4183247a2f45aa17c6fdc9798f3039352eb4e
@hexlet/cli <support@hexlet.io>
2024-02-01 19:10:13 +0600

    submit ruby-basics-ru/oop

---
+++ ruby-basics-ru/oop/lib/url.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# BEGIN
+require 'uri'
+require 'forwardable'
+
+class Url
+  extend Forwardable
+  include Comparable
+
+  def initialize(url)
+    @url = URI(url)
+  end
+
+  def_delegators :@url, :scheme, :host, :port
+
+  def query_params
+    query_as_hash(@url.query)
+  end
+
+  def query_param(key, value = nil)
+    query = query_as_hash(@url.query)
+    query.key?(key) ? query[key] : value
+  end
+
+  def ==(other)
+    (@url.host == other.host && @url.scheme == other.scheme) &&
+      (@url.port == other.port && query_params == other.query_params)
+  end
+
+  private
+
+  def query_as_hash(query)
+    query.split('&').each_with_object({}) do |elem, acc|
+      key, value = elem.split('=')
+      acc.merge!({ key.to_sym => value })
+    end.sort.to_h
+  end
+end
+# END
